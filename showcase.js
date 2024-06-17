2101fdd437ec3ad0b9f1f83538d6b0dd198b0950
Juan Pablo Patino <patinoart@gmail.com>
2024-06-17 14:42:12 -0400

    Update showcase.js
    removed autoplay

--- showcase.js
+++ showcase.js
@@ -10,48 +10,42 @@ $(document).ready(function () {
             prevEl: '.prev-slide'
         },
         pagination: {
+            type: 'bullets',
             el: '.pagination',
             clickable: true,
-            renderBullet: function (index, className) {
-                return '<span class="' + className + '">' + (index + 1) + '</span>';
-            },
 
         },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false,
+        // autoplay: {
+        //     delay: 3000,
+        //     disableOnInteraction: false,
 
-        },
-        speed: 500,
+        // },
+        speed: 700,
         slidesPerView: 'auto',
         loop: true,
 
         on: {
             init: function () {
                 var swiper_pagination_bulletwe_style = document.createElement('style'); swiper_pagination_bulletwe_style.type = 'text/css'; swiper_pagination_bulletwe_style.innerHTML =
-                    ` .swiper-pagination-bullet{   
-                        background:#000;
-                        margin-right:8px;
-                        transition:.2s;
-                    } `;
+                    ` .swiper-pagination-bullet{   background:#fff;
+  margin-right:8px;
+  transition:.2s;
+ } `;
                 document.getElementsByTagName('head')[0].appendChild(swiper_pagination_bulletwe_style); var swiper_pagination_bullethoverwe_style = document.createElement('style'); swiper_pagination_bullethoverwe_style.type = 'text/css'; swiper_pagination_bullethoverwe_style.innerHTML =
-                    ` .swiper-pagination-bullet:hover{   
-                        opacity:.7;
-                    } `;
+                    ` .swiper-pagination-bullet:hover{   opacity:.7;
+ } `;
                 document.getElementsByTagName('head')[0].appendChild(swiper_pagination_bullethoverwe_style); var swiper_pagination_bullet_activehoverwe_style = document.createElement('style'); swiper_pagination_bullet_activehoverwe_style.type = 'text/css'; swiper_pagination_bullet_activehoverwe_style.innerHTML =
-                    ` .swiper-pagination-bullet-active:hover{   
-                        opacity:1;
-                    } `;
+                    ` .swiper-pagination-bullet-active:hover{   opacity:1;
+ } `;
                 document.getElementsByTagName('head')[0].appendChild(swiper_pagination_bullet_activehoverwe_style); var swiper_pagination_fractionwe_style = document.createElement('style'); swiper_pagination_fractionwe_style.type = 'text/css'; swiper_pagination_fractionwe_style.innerHTML =
-                    ` .swiper-pagination-fraction{   
-                        font-size:16px;
-                    } `;
+                    ` .swiper-pagination-fraction{   font-size:16px;
+ } `;
                 document.getElementsByTagName('head')[0].appendChild(swiper_pagination_fractionwe_style); var slide_imagewe_style = document.createElement('style'); slide_imagewe_style.type = 'text/css'; slide_imagewe_style.innerHTML =
                     ` .slide-image{   transition:transform .2s;
-                    } `;
+ } `;
                 document.getElementsByTagName('head')[0].appendChild(slide_imagewe_style); var slide_contentwe_style = document.createElement('style'); slide_contentwe_style.type = 'text/css'; slide_contentwe_style.innerHTML =
                     ` .slide-content{   transition:opacity .2s;
-                    } `;
+ } `;
                 document.getElementsByTagName('head')[0].appendChild(slide_contentwe_style); $('.slider-item').on('mouseover', function () {
                     $(this).find('.slide-image').css({
                         'transform': 'scale(1.1)',
